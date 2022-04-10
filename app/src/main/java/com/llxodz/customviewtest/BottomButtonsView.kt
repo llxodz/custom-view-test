9e9c7fac585975f3a7d621b36219fe2dd48faf76
Ilya Gavrilov <llxodz@gmail.com>
2022-04-10 03:56:27 +0300

    Created functionality for saving/restoring the View state

--- app/src/main/java/com/llxodz/customviewtest/BottomButtonsView.kt
+++ app/src/main/java/com/llxodz/customviewtest/BottomButtonsView.kt
@@ -3,6 +3,8 @@ package com.llxodz.customviewtest
 import android.content.Context
 import android.content.res.ColorStateList
 import android.graphics.Color
+import android.os.Parcel
+import android.os.Parcelable
 import android.util.AttributeSet
 import android.view.LayoutInflater
 import android.view.View
@@ -25,6 +27,7 @@ class BottomButtonsView(
 
     private val binding: PartButtonsBinding
     private var listener: OnBottomButtonsActionListener? = null
+
     var isProgressMode: Boolean = false
         set(value) {
             field = value
@@ -123,5 +126,51 @@ class BottomButtonsView(
 
     fun setNegativeButtonText(text: String?) {
         binding.negativeButton.text = text
+    }
+
+    override fun onSaveInstanceState(): Parcelable? {
+        val superState = super.onSaveInstanceState()!!
+        val savedState = SavedState(superState)
+        savedState.positiveButtonText = binding.positiveButton.text.toString()
+        savedState.negativeButtonText = binding.negativeButton.text.toString()
+        return savedState
+    }
+
+    override fun onRestoreInstanceState(state: Parcelable?) {
+        val savedState = state as SavedState
+        super.onRestoreInstanceState(savedState.superState)
+        binding.positiveButton.text = savedState.positiveButtonText
+        binding.negativeButton.text = savedState.negativeButtonText
+    }
+
+    class SavedState : BaseSavedState {
+
+        var positiveButtonText: String? = null
+        var negativeButtonText: String? = null
+
+        constructor(superState: Parcelable) : super(superState)
+        constructor(parcel: Parcel) : super(parcel) {
+            positiveButtonText = parcel.readString()
+            negativeButtonText = parcel.readString()
+        }
+
+        override fun writeToParcel(out: Parcel, flags: Int) {
+            super.writeToParcel(out, flags)
+            out.writeString(positiveButtonText)
+            out.writeString(negativeButtonText)
+        }
+
+        companion object {
+            @JvmField
+            val CREATOR: Parcelable.Creator<SavedState> = object : Parcelable.Creator<SavedState> {
+                override fun createFromParcel(source: Parcel): SavedState {
+                    return SavedState(source)
+                }
+
+                override fun newArray(size: Int): Array<SavedState?> {
+                    return Array(size) { null }
+                }
+            }
+        }
     }
 }
--- app/src/main/java/com/llxodz/customviewtest/MainActivity.kt
+++ app/src/main/java/com/llxodz/customviewtest/MainActivity.kt
@@ -3,9 +3,6 @@ package com.llxodz.customviewtest
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
-import android.widget.Toast
 import androidx.annotation.RequiresApi
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.llxodz.customviewtest.databinding.ActivityMainBinding
